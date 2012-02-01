83dc2490c9180f1c8637eb7cb2389ff182f9ccdb
George DelMonte <grd7y@virginia.edu>
2012-02-01 10:30:19 -0500

    Added extras

---
+++ src/com/willowtree/pilottime/PilotTimeRefs.java
@@ -0,0 +1,14 @@
+package com.willowtree.pilottime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: george
+ * Date: 2/1/12
+ * Time: 9:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PilotTimeRefs {
+    public static final String MAIN_TYPE = "main";
+    public static final String RESULT_TYPE = "result";
+    public static final String BASE_TYPE = "base";
+}
